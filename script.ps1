ea9e1502614161a15a0a7d97f5adf632cb27afe0
ch010060 <ch010060@gmail.com>
2022-02-09 10:59:10 +0800

    Skip bif generation of noninitial part file.

--- script.ps1
+++ script.ps1
@@ -9,11 +9,19 @@ foreach ($file in $files)
     $filedir = $file.DirectoryName
     $BIF_PATH = "${filedir}\${filenameBase}-320-10.bif"
     echo "($counter/$totalNum) $filenameFull"
-    if(Test-Path -Path $BIF_PATH -PathType Leaf){
-        echo "BIF file does exist, skip !"
+
+    # Skip bif generation of noninitial part file.
+    if(-not($filenameBase -match '.*-[cC][dD][2-9]')){
+        # Skip bif generation if bif file does exist.
+        if(Test-Path -Path $BIF_PATH -PathType Leaf){
+            echo "BIF file does exist, skip !"
+        }
+        else {
+            python3 bifgen.py -o $BIF_PATH "${filenameFull}"
+        }
     }
-    else {
-        python3 bifgen.py -o $BIF_PATH "${filenameFull}"
+    else{
+        echo "Skip noninitial part file !"
     }
     $counter++
 }
